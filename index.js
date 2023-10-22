7097195bc885a461618f21213787914ec3d64a9d
tahmid530 <tahmidahmed530@gmail.com>
2023-10-22 11:34:52 +0600

    data create sucessfully

--- index.js
+++ index.js
@@ -12,7 +12,6 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rd118yc.mongodb.net/?retryWrites=true&w=majority`;
 
-
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
@@ -26,12 +25,23 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const productsCollection = client.db('productsDB').collection('products');
+
+    app.post('/products', async (req, res) => {
+        const newProducts = req.body;
+        console.log(newProducts);
+        const result = await productsCollection.insertOne(newProducts);
+        res.send(result);
+    })
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
