60152893116fd4e28a1dba3db3e00b6085627085
tahmid530 <tahmidahmed530@gmail.com>
2023-10-22 11:59:50 +0600

    data read sucessfully

--- index.js
+++ index.js
@@ -28,6 +28,12 @@ async function run() {
 
     const productsCollection = client.db('productsDB').collection('products');
 
+    app.get('/products', async (req, res) => {
+        const cursor = productsCollection.find();
+        const result = await cursor.toArray();
+        res.send(result);
+    })
+
     app.post('/products', async (req, res) => {
         const newProducts = req.body;
         console.log(newProducts);
