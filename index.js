5b64c1174a41ee7d0835866b1b564bd3801b7283
tahmid530 <tahmidahmed530@gmail.com>
2023-10-25 22:49:12 +0600

    bug fixed

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express');
+const app = express();
 const cors = require('cors');
 require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
@@ -110,7 +110,7 @@ async function run() {
 
 
         // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
+        // await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
